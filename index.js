b557ebefb6ecfbb09746d0ee238aad77d5692747
Way-Maker7 <Denis_Sabou@gmx.de>
2022-08-25 19:47:01 +0200

    click-event

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ window.addEventListener("scroll", () => {
     nav.style.top = "-50px";
   }
 });
+
+const questionContainer = document.querySelector(".click-event");
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.querySelector("#btn-2");
+const answer = document.querySelector("p");
+
+questionContainer.addEventListener("click", () => {
+  questionContainer.classList.toggle("question-clicked");
+});
+
+btn1.addEventListener("click", () => {
+  answer.classList.add("show-answer");
+  answer.style.background = "red";
+});
+
+btn2.addEventListener("click", () => {
+  answer.classList.add("show-answer");
+  answer.style.background = "green";
+});
